c2b75a8052416f9fda156b13c764a6a57a8e4d44
ldaltamirano <lalt123@gmail.com>
2018-10-08 19:19:51 -0300

    Cambios

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
+<?php 
+	//include( 'config/arrays.php' );
+	//include( '../config/config.php' );
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>todo Deporte</title>
+	<link rel="stylesheet" href="css/estilos.css" />
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/bootstrap.css" />
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+	<!--<script src="../js/jquery.js"></script>
+	<script src="../js/bootstrap.js"></script> -->
 </head>
 <body>
-    
+	
 </body>
 </html>
