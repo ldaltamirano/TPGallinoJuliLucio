030659f75eec439fc50a34e346617bdf05f6f21b
ldaltamirano <lalt123@gmail.com>
2018-10-11 14:10:00 -0300

    cambios en links

--- js/acciones.js
+++ js/acciones.js
@@ -1,4 +1,19 @@
 window.addEventListener('DOMContentLoaded', function() {
-    
-
+    let links = document.querySelectorAll('.verNoti');
+    links.forEach(function(link) {
+        link.addEventListener('click', function(ev) {
+            ev.preventDefault();
+            let idPelicula = link.getAttribute('data-id');
+            console.log("Clickeamos un link! ID: ", idPelicula);
+            ajax({
+                method: 'GET',
+                url: 'api/ver-pelicula.php',
+                data: 'id=' + idPelicula,
+                successCallback: function(rta) {
+                    // TODO: Mostrar los datos.
+                    console.log("Ver: ", rta);
+                }
+            });
+        });
+    });
 });
